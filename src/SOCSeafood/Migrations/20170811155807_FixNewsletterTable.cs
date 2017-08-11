e9b204cc1aee5f6ed11aad12cb0b3860b129de5d
Pete Lazuran <pdlazuran@gmail.com>
2017-08-11 09:09:06 -0700

    add newsletter model, new migrations, shared layout

---
+++ src/SOCSeafood/Migrations/20170811155807_FixNewsletterTable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SOCSeafood.Migrations
+{
+    public partial class FixNewsletterTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ src/SOCSeafood/Models/Newsletter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SOCSeafood.Models
+{
+    [Table("NewsletterSubscribers")]
+    public class Newsletter
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Interest { get; set; }
+        public bool IsSignedUp { get; set;}
+        public DateTime SubscribeDate { get; set; }
+        public virtual ApplicationUser User { get; set;}
+    }
+}
