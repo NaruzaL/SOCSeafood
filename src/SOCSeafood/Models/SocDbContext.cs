17c8274844a1cf42c043a8afb5e55bf5e2ec9b18
Pete Lazuran <pdlazuran@gmail.com>
2017-08-11 08:37:50 -0700

    add user Authentication

---
+++ src/SOCSeafood/Models/SocDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace SOCSeafood.Models
+{
+    public class SocDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public SocDbContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SocSeafood;integrated security=True");
+        }
+        public SocDbContext(DbContextOptions<SocDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
