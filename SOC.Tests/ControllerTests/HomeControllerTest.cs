d5b49f4ac312746a38d5157c1194cde5fee7a4ef
Pete Lazuran <pdlazuran@gmail.com>
2017-08-11 10:45:47 -0700

    create and run test for Homecontroller Index, passing 2/2 tests.

---
+++ SOC.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SOCSeafood.Controllers;
+using SOCSeafood.Models;
+using Xunit;
+namespace SOC.Tests.ControllerTests
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void Get_ViewResult_Signup_Test()
+        {
+            //Arrange
+            HomeController controller = new HomeController();
+
+            //Act
+            var result = controller.Index();
+
+            //Assert
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
